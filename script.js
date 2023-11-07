b00915f3c9c5d1c637da47757a51b693f28e063c
Krishna <Krishnasrinivasan41@gmail.com>
2023-11-07 12:35:20 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,218 @@
+let iframe=document.getElementById("frame");
+let rightFrame=document.getElementById("rightFrame");
+let menu=document.getElementById("menu");
+let certificate=document.getElementById("certificate");
+
+function introFunc(){
+    iframe.src="intro/intro.html";
+    iframe.style.minHeight="159vh";
+}
+
+
+function formatBFunc(){
+    iframe.src="basics/bold.html";
+    iframe.style.minHeight="195vh";
+}
+function formatStrongFunc(){
+    iframe.src="basics/strong.html";
+    iframe.style.minHeight="194vh";
+}
+function formatIFunc(){
+    iframe.src="basics/italic.html";
+    iframe.style.minHeight="192vh";
+}
+function formatEmFunc(){
+    iframe.src="basics/emphasis.html";
+    iframe.style.minHeight="192vh";
+}
+function formatMarkFunc(){
+    iframe.src="basics/marked.html";
+    iframe.style.minHeight="189vh";
+}
+function formatUFunc(){
+    iframe.src="basics/underlined.html";
+    iframe.style.minHeight="188.2vh";
+}
+function formatStrikeFunc(){
+    iframe.src="basics/strike.html";
+    iframe.style.minHeight="192vh";
+}
+function formatSupFunc(){
+    iframe.src="basics/sup.html";
+    iframe.style.minHeight="192vh";
+}
+function formatSubFunc(){
+    iframe.src="basics/sub.html";
+    iframe.style.minHeight="192vh";
+}
+function formatDelFunc(){
+    iframe.src="basics/del.html";
+    iframe.style.minHeight="192vh";
+}
+function formatInsFunc(){
+    iframe.src="basics/ins.html";
+    iframe.style.minHeight="192vh";
+}
+function formatBigFunc(){
+    iframe.src="basics/big.html";
+    iframe.style.minHeight="192vh";
+}
+function formatSmallFunc(){
+    iframe.src="basics/small.html";
+    iframe.style.minHeight="192vh";
+}
+
+
+function groupDivFunc(){
+    iframe.src="group/div.html";
+    iframe.style.minHeight="199vh";
+}
+function groupSpanFunc(){
+    iframe.src="group/span.html";
+    iframe.style.minHeight="199vh";
+}
+
+
+function listTypeFunc(){
+    iframe.src="list/types.html";
+}
+function listOlFunc(){
+    iframe.src="list/ol.html";
+    iframe.style.minHeight="205vh";
+}
+function listUlFunc(){
+    iframe.src="list/ul.html";
+    iframe.style.minHeight="205vh";
+}
+function listDlFunc(){
+    iframe.src="list/dl.html";
+    iframe.style.minHeight="225vh";
+}
+
+
+function imgFunct(){
+    iframe.src="ImageF/img.html";
+    iframe.style.minHeight="216vh";
+}
+function imgMapFunct(){
+    iframe.src="ImageF/imageMap.html";
+    iframe.style.minHeight="193vh";
+}
+
+
+function hLinkFunct(){
+    iframe.src="hlink/aTag.html";
+    iframe.style.minHeight="195vh";
+}
+function intLinkFunct(){
+    iframe.src="hlink/intLink.html";
+    iframe.style.minHeight="195vh";
+}
+function extLinkFunct(){
+    iframe.src="hlink/extLink.html";
+    iframe.style.minHeight="185vh";
+}
+
+
+function tableFunct(){
+    iframe.src="table/table.html";
+    iframe.style.minHeight="218vh";
+}
+function tableTrFunct(){
+    iframe.src="table/tr.html";
+    iframe.style.minHeight="157vh";
+}
+function tableThFunct(){
+    iframe.src="table/th.html";
+    iframe.style.minHeight="157vh";
+}
+function tableTdFunct(){
+    iframe.src="table/td.html";
+    iframe.style.minHeight="157vh";
+}
+function tableTheadFunct(){
+    iframe.src="table/thead.html";
+    iframe.style.minHeight="159vh";
+}
+function tableTbodyFunct(){
+    iframe.src="table/tbody.html";
+    iframe.style.minHeight="157vh";
+}
+function tableTfootFunct(){
+    iframe.src="table/tfoot.html";
+    iframe.style.minHeight="157vh";
+}
+function tableCaptionFunct(){
+    iframe.src="table/caption.html";
+    iframe.style.minHeight="157vh";
+}
+function tableColspanFunct(){
+    iframe.src="table/colspan.html";
+    iframe.style.minHeight="157vh";
+}
+function tableRowspanFunct(){
+    iframe.src="table/rowspan.html";
+    iframe.style.minHeight="157vh";
+}
+
+
+function commentFunct(){
+    iframe.src="comment/comments.html";
+}
+
+
+function formInputFunct(){
+    iframe.src="form/input.html";
+    iframe.style.minHeight="207vh";
+}
+function formTextAreaFunct(){
+    iframe.src="form/textarea.html";
+    iframe.style.minHeight="217vh";
+}
+function formButtonFunct(){
+    iframe.src="form/button.html";
+    iframe.style.minHeight="208vh";
+}
+function formSelectFunct(){
+    iframe.src="form/select.html";
+    iframe.style.minHeight="185vh";
+}
+function formLabelFunct(){
+    iframe.src="form/label.html";
+    iframe.style.minHeight="176vh";
+}
+
+
+function headerTitleFunct(){
+    iframe.src="header/title.html";
+    iframe.style.minHeight="225vh";
+}
+function headerLinkFunct(){
+    iframe.src="header/link.html";
+    iframe.style.minHeight="155vh";
+}
+function headerStyleFunct(){
+    iframe.src="header/style.html";
+    iframe.style.minHeight="155vh";
+}
+function headerScriptFunct(){
+    iframe.src="header/script.html";
+    iframe.style.minHeight="155vh";
+}
+function headerMetaFunct(){
+    iframe.src="header/meta.html";
+    iframe.style.minHeight="390vh";
+}
+
+
+
+
+function formatFunc(){
+    iframe.src="intro/intro.html";
+}
+
+function getCertified(){
+    menu.style.display="none";
+    iframe.style.display="none"
+    certificate.style.display="block";
+}
